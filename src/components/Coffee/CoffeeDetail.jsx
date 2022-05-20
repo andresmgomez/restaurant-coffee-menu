9c6f9824999709bd8309199fa594ab0cd0744529
Miguel Gomez <andresmgomez23@gmail.com>
2022-05-20 05:14:45 -0400

    Fetch the list of coffees' title and description from the API

--- src/components/Coffee/CoffeeDetail.jsx
+++ src/components/Coffee/CoffeeDetail.jsx
@@ -1,11 +1,11 @@
 import './CoffeeDetail.css'
 
-export default function CoffeeDetail() {
+export default function CoffeeDetail(props) {
    return (
       <>
          <div className="coffee-details">
-            <h4 className="coffee-title">Coffee Title</h4>
-            <p className="coffee-description">Coffee Description</p>
+            <h4 className="coffee-title">{props.title}</h4>
+            <p className="coffee-description">{props.description}</p>
          </div>
       </>
    )
--- src/components/Coffees/CoffeeList.jsx
+++ src/components/Coffees/CoffeeList.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
 import CoffeeDetail from "../Coffee/CoffeeDetail";
 import './CoffeeList.css'
 
 export default function CoffeeList() {
+   // Set default state for our list of coffees
+   const [coffeeList, setCoffeeList] = useState([]);
+
+   useEffect(() => {
+      // Fetch the list of coffees from the REST API
+      const getCoffeeAPI = async () => {
+         const response = await fetch(`https://api.sampleapis.com/coffee/hot`)
+         const hotCoffees = await response.json();
+         await setCoffeeList(hotCoffees);
+      }
+      getCoffeeAPI();
+   }, [])
+
    return (
       <>
          <div className="row">
-            <div className="col-12 col-lg-6 col-xl-3">
-               <article className="coffee-list">
-                  <CoffeeDetail />
-                  <CoffeeDetail />
-               </article>
+            <div className="col-sm-12 col-lg-6 col-xl-3">
+               {coffeeList && (
+                  <article className="coffee-list">
+                     {coffeeList.map(eachCoffee => {
+                        return <CoffeeDetail title={eachCoffee.title} description={eachCoffee.description} />
+                     })}
+                  </article>
+               )}
             </div>
          </div>
       </>
